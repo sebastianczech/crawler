7ae0eac88ef3a8e71ad001ab57ac8118f41411ee
Sebastian Czech <sebaczech@gmail.com>
2019-03-19 23:10:40 +0100

    feature: Checking in list of all links if this link was analysed

--- crawler.py
+++ crawler.py
@@ -4,40 +4,43 @@ from urllib.parse import urlparse
 import re
 from page import Page
 
-def is_absolute(url):
-    return bool(urlparse(url).netloc)
+all_links = []
 
-def get_list_of_links_for_domain(url, content):
+def get_list_of_links_for_domain(url, domain, content, level):
     links = []
     resources = []
     parsed_url = urlparse(url)
     for line in content.splitlines():
-        get_links_from_content_line(line, "href", links, parsed_url)
-        get_links_from_content_line(line, "src", resources, parsed_url)
+        get_links_from_content_line(line, "href", links, parsed_url, domain, level)
+        get_links_from_content_line(line, "src", resources, parsed_url, domain, level)
     return links
 
 
-def get_links_from_content_line(line, type, links, parsed_url):
+def get_links_from_content_line(line, type, links, parsed_url, domain, level):
     for part in re.split(type, line):
         if part.startswith("=\"") and len(part.replace("=", "").split("\"")) > 0:
             link = part.replace("=", "").split("\"")[1]
-            if is_absolute(link) and len(parsed_url.netloc) > 0 and parsed_url.netloc.replace("www.", "") in link \
-                    or not is_absolute(link):
-                page = Page(link, type)
-                try:
-                    r = requests.get(link)
-                    # if r.status_code == 200 and "text/html" in r.headers['Content-Type']:
-                    #         return get_list_of_links_for_domain(url, r.text)
-                except:
-                    pass
-                links.append(page)
+            if Page.is_absolute(link) and domain in link or not Page.is_absolute(link):
+                page = Page(link, type, parsed_url.netloc, level)
+                if page not in all_links and len(all_links) < 50:
+                    links.append(page)
+                    all_links.append(page)
+                    try:
+                        print("GET " + page.absolute_url)
+                        r = requests.get(page.absolute_url)
+                        if r.status_code == 200 and "text/html" in r.headers['Content-Type']\
+                                and domain in page.absolute_url:
+                            page.sublinks = get_list_of_links_for_domain(page.absolute_url, domain, r.text, level+1)
+                    except:
+                        pass
 
 def parse_html_page(url):
     try:
+        print("GET " + url)
         r = requests.get(url)
         if r.status_code == 200:
             if "text/html" in r.headers['Content-Type']:
-                return get_list_of_links_for_domain(url, r.text)
+                return get_list_of_links_for_domain(url, urlparse(url).netloc.replace("www.", ""), r.text, 0)
             else:
                 return "Incorrect content type " + r.headers['Content-Type'] + " in response instead of text/html"
         else:
@@ -53,7 +56,7 @@ if __name__ == '__main__':
     else:
         parsed_data = parse_html_page(sys.argv[1])
         if type(parsed_data) is list:
-            print("Simple structured site map for " + sys.argv[1] + ":\n")
+            print("\nSimple structured site map for " + sys.argv[1] + ":\n")
             for page in parsed_data:
                 print(page)
         else:
